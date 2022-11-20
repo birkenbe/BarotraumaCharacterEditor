186241745ef4a5bc558f05a4678634f332d30208
Martin <martin.birken@gmail.com>
2022-11-20 13:11:50 +0100

    Added test to confirm generated classes are working

---
+++ src/api/BarotraumaCharacterEditor.APIUnitTests/Models/CharacterDataTest.cs
@@ -0,0 +1,24 @@
+﻿using BarotraumaCharacterEditor.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BarotraumaCharacterEditor.APIUnitTests.Models
+{
+    public class CharacterDataTest
+    {
+        [Fact]
+        public void Unmarshal_characterdata()
+        {
+            XmlSerializer reader = new XmlSerializer(typeof(CharacterData));
+            StreamReader file = new StreamReader(@"Resources\CharacterData.xml");
+            CharacterData data = (CharacterData)reader.Deserialize(file);
+            file.Close();
+
+            Assert.Matches("Timo", data.CharacterCampaignData[0].name);
+        }
+    }
+}
