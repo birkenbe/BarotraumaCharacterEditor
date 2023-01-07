f2db760c39f41815f02dbae76912915ad2bcc21f
Martin <martin.birken@gmail.com>
2023-01-07 18:41:00 +0100

    Fix DB seeding on startup

---
+++ src/api/BarotraumaCharacterEditor.API/Migrations/20230105125709_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarotraumaCharacterEditor.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CharacterDataBlobs",
+                columns: table => new
+                {
+                    Hash = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CharacterDataBlobs", x => x.Hash);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CharacterDataBlobs");
+        }
+    }
+}
--- src/api/BarotraumaCharacterEditor.API/Program.cs
+++ src/api/BarotraumaCharacterEditor.API/Program.cs
@@ -5,13 +5,14 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString= builder.Configuration.GetConnectionString("CharacterBlobDatabase");
+builder.Services.AddDbContext<CharacterDataBlobContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddScoped<ICharacterDataBlobRepository, CharacterDataBlobRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ICharacterDataBlobRepository, CharacterDataBlobRepository>();
-builder.Services.AddDbContext<CharacterDataBlobContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CharacterBlobDatabase")));
 
 var app = builder.Build();
 
@@ -28,18 +29,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-//try
-//{
-//    var dbContext = app.Services.GetRequiredService<CharacterDataBlobContext>();
-//    dbContext.Database.Migrate();
-//}
-//catch (Exception ex)
-//{
-//    var logger = app.Logger;
+try
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<CharacterDataBlobContext>();
+    dbContext.Database.Migrate();
+}
+catch (Exception ex)
+{
+    ILogger logger = app.Logger;
 
-//    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
 
-//    throw;
-//}
+    throw;
+}
 
 app.Run();
