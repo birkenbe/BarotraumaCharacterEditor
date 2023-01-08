fbf9de3d22e188340aeffe4420849fa9abc18113
Martin <martin.birken@gmail.com>
2023-01-08 17:56:13 +0100

    add job class (kinda enum)

---
+++ src/api/BarotraumaCharacterEditor.API/Models/Job.cs
@@ -0,0 +1,12 @@
+﻿namespace BarotraumaCharacterEditor.API.Models
+{
+    public class Job
+    {
+        public readonly static (string name, string qualifier) Assistant = ("Assistant", "assistant");
+        public readonly static (string name, string qualifier) Captain = ("Captain", "captain");
+        public readonly static (string name, string qualifier) Engineer = ("Engineer", "engineer");
+        public readonly static (string name, string qualifier) Mechanic = ("Mechanic", "mechanic");
+        public readonly static (string name, string qualifier) Medic = ("Medical Doctor", "medicaldoctor");
+        public readonly static (string name, string qualifier) SecurityOfficer = ("Security Officer", "securityofficer");
+    }
+}
